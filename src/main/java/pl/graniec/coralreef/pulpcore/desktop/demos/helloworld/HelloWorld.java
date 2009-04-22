ace1e8a17ea90168b9ac49b07e09d8fe9f290db0
chudy <chudy@2f5b2cf6-057d-48ac-b1f1-6de7f60f437b>
2009-04-22 18:10:16 +0000

    * Created Hello World demo
    git-svn-id: svn://atom/general/CoralReef/trunk/cr-pulpcore-desktop-demo@145 2f5b2cf6-057d-48ac-b1f1-6de7f60f437b

---
+++ src/main/java/pl/graniec/coralreef/pulpcore/desktop/demos/helloworld/HelloWorld.java
@@ -0,0 +1,59 @@
+/**
+ * Copyright (c) 2009, Coral Reef Project
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ *  * Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *  * Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *  * Neither the name of the Coral Reef Project nor the names of its
+ *    contributors may be used to endorse or promote products derived from this
+ *    software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+package pl.graniec.coralreef.pulpcore.desktop.demos.helloworld;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import pl.graniec.pulpcore.desktop.CoreApplication;
+
+/**
+ * @author Piotr Korzuszek <piotr.korzuszek@gmail.com>
+ *
+ */
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		
+		// When PulpCore is run as a applet it can receive parameters from
+		// browser (key => value) that is runs in. Params of application is
+		// something little different so you have a free choice what you want
+		// to pass further.
+		final Map<String, String> properties = new HashMap<String, String>();
+		
+		// Most important parameter that should be set is first scene class.
+		// This is the only thing you should do before PulpCore will run
+		// on your desktop.
+		properties.put(CoreApplication.FIRST_SCENE_CLASS_PROPERTY, HelloWorldScene.class.getName());
+		
+		// This will finally run the PulpCore!
+		new CoreApplication(properties).run();
+	}
+
+}
---
+++ src/main/java/pl/graniec/coralreef/pulpcore/desktop/demos/helloworld/HelloWorldScene.java
@@ -0,0 +1,58 @@
+/**
+ * Copyright (c) 2009, Coral Reef Project
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ *  * Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *  * Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *  * Neither the name of the Coral Reef Project nor the names of its
+ *    contributors may be used to endorse or promote products derived from this
+ *    software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+package pl.graniec.coralreef.pulpcore.desktop.demos.helloworld;
+
+import static pulpcore.image.Colors.BLACK;
+import pulpcore.image.CoreFont;
+import pulpcore.scene.Scene2D;
+import pulpcore.sprite.FilledSprite;
+import pulpcore.sprite.Label;
+
+/**
+ * @author Piotr Korzuszek <piotr.korzuszek@gmail.com>
+ *
+ */
+public class HelloWorldScene extends Scene2D {
+	
+	CoreFont textFont = CoreFont.getSystemFont().tint(0xFFFFFFFF);
+	Label helloWorldLabel;
+	
+	@Override
+	public void load() {
+		// creates 'Hello World' label in the center
+		helloWorldLabel = new Label(textFont, "Hello World", 320, 240);
+		helloWorldLabel.anchorX.set(0.5);
+		helloWorldLabel.anchorY.set(0.5);
+		
+		// add background
+		add(new FilledSprite(BLACK));
+		// and the label
+		add(helloWorldLabel);
+	}
+}
